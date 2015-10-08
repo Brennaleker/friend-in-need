daab2da44935ef6c8dfe28fbcf3c17796e3dbec9
Brenna <brennaleker@gmail.com>
2015-10-08 13:52:05 -0700

    added schema and index.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var knex = require('knex')({
     client: 'mysql',
     connection: {
         host     : '127.0.0.1',
-        user     : 'your_database_user',
-        password : 'your_database_password',
-        database : 'myapp_test',
+        user     : 'Brenna',
+        password : null,
+        database : 'shelter_helper',
         charset  : 'utf8'
   }
 });
