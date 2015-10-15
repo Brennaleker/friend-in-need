cb226198c736faf116b4eddc1bf4a5ca503662fa
Brenna <brennaleker@gmail.com>
2015-10-14 17:04:04 -0700

    one last commit before I try registry plugin

--- models/user.js
+++ models/user.js
@@ -1,23 +1,24 @@
 var knex = require('../db/knexfile.js')
     Bookshelf = require('bookshelf')(knex),
-    Organization = require('./organization.js'),
-    Volunteer = require('./volunteer.js'),
+    // Bookshelf.plugin('registry'),
+    // Organization = require('./organization.js'),
+    // Volunteer = require('./volunteer.js'),
     Donor = require('./donor.js');
 
 var User = Bookshelf.Model.extend({
     tableName: 'users',
     donor: function(){
       return this.hasOne(Donor);
-    },
-    volunteer: function(){
-      return this.hasOne(Volunteer);
-    },
-    organiztion: function(){
-      return this.hasOne(Organization);
-    },
-    project: function(){
-      return this.hasMany(Project);
     }
+    // volunteer: function(){
+    //   return this.hasOne(Volunteer);
+    // },
+    // organiztion: function(){
+    //   return this.hasOne(Organization);
+    // },
+    // project: function(){
+    //   return this.hasMany(Project);
+    // }
 });
 
 
--- test/models/user.js
+++ test/models/user.js
@@ -7,15 +7,15 @@ var chai = require('chai'),
     Volunteer = require('../../models/donor.js'),
     User = require('../../models/user.js');
 chai.use(require('chai-bookshelf'));
-
+//
 describe('User Relationships', function() {
   it('has one donor', function() {
     expect(User).to.haveOne(Donor);
   })
-  it('has one organization', function() {
-    expect(User).to.haveOne(Organization);
-  }),
-  it('has one volunteer', function() {
-    expect(User).to.haveOne(Volunteer);
-  })
+//   it('has one organization', function() {
+//     expect(User).to.haveOne(Organization);
+//   }),
+//   it('has one volunteer', function() {
+//     expect(User).to.haveOne(Volunteer);
+//   })
 });
